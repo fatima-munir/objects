d7a001f4d3854e1fe700909b2de8e04a64cbbf7a
Fatima Munir <129934855+fatima-munir@users.noreply.github.com>
2023-04-07 20:03:42 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,54 @@
+let name = 'sara'; // variable declaration and assignment
+console.log(name);// for display on console
+let age = 30; // int var assignment
+let isApproved = false; // boolean variable assignment 0/1
+let firstName = undefined; 
+let selectedcolor = null;
+let person = {                                                                            
+    name:'hajra',
+    age: 14
+}; 
+// Dot Notation
+person.name = 'john';
+person.age = 30;
+
+// Bracket Notation
+//person['name'] = 'mary';
+
+console.log(person);
+let selectcolor = ['red','yellow','blue','green',91,'purple'];
+console.log(selectcolor[4]);
+console.log(selectcolor.length);
+let interestrate = 0.3;
+interestrate = 2;
+let car = {stearing: 'toyota',
+model = 'honda'
+year: 2008,
+doors: 2};
+console.log(car);
+
+let person = {
+    name: 'sara',
+    age: 10
+};
+console.log('person');
+let x = 10;
+let y = 3;
+console.log(x*y)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
